e1b8371aa6d7bf684ff01b61005b6c6bbf19cefe
hussainwali74 <13bscshwali@seecs.edu.pk>
2020-04-26 22:44:36 +0500

    structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './app.module.css';
+class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <h1>App</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+
+        )
+    }
 }
+export default App;
